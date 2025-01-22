952b9e47c6ee563c5cd8ecc51a915cf68abebcb7
Talon Gobel <talon.gobel@gmail.com>
2025-01-22 15:13:24 -0500

    1-22

--- ros2_ws/Lab1_ws/task_1/setup.py
+++ ros2_ws/Lab1_ws/task_1/setup.py
@@ -20,6 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'talker = task_1.my_first_topic:main',
+            'listener = task_1.my_first_subscriber:main',
         ],
     },
 )
